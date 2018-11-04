4129684970ff19d6f6f5ff80108068e564f77639
samuel <samuel@intsights.com>
2018-11-04 18:47:37 +0200

    Add Hot Module for webpack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {}
+export class AppComponent {
+    name = 'Samuel';
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,4 @@ const environment = (process.env.NODE_ENV || 'development').trim();
 
 if (environment === 'development') {
     module.exports = require('./config/webpack.config.dev');
-} else if (environment === 'production') {
-
 }
